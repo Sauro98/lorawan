a5611ee0b5c2eedec12b25446b9b9d0c673c9364
Ivano Donadi <ivadonadi98@gmail.com>
2016-08-23 13:00:25 +0200

    changed types

--- Raspberry/dist/json/json.h
+++ Raspberry/dist/json/json.h
@@ -267,8 +267,8 @@ typedef unsigned int LargestUInt;
 #else                 // if defined(JSON_NO_INT64)
 // For Microsoft Visual use specific types as long long is not supported
 #if defined(_MSC_VER) // Microsoft Visual Studio
-typedef __int64 Int64;
-typedef unsigned __int64 UInt64;
+typedef int64_t Int64;
+typedef uint64_t UInt64;
 #else                 // if defined(_MSC_VER) // Other platforms, use long long
 typedef int64_t Int64;
 typedef uint64_t UInt64;
