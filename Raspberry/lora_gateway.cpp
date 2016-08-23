574002e6afedccc57a5b56f954d28e7cfa9a9a53
Ivano Donadi <ivadonadi98@gmail.com>
2016-08-23 15:57:42 +0200

    declare at the top

--- Raspberry/lora_gateway.cpp
+++ Raspberry/lora_gateway.cpp
@@ -370,6 +370,8 @@ CURLcode res;
 
 #pragma region functions
 
+bool sendDBContent();
+
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
 	((std::string*)userp)->append((char*)contents, size * nmemb);
