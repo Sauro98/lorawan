e728c0469a9f91450a986ac2cebfd81706c129dd
Ivano Donadi <ivadonadi98@gmail.com>
2016-08-23 15:37:41 +0200

    missing a )

--- Raspberry/lora_gateway.cpp
+++ Raspberry/lora_gateway.cpp
@@ -956,7 +956,7 @@ void loop(void)
 		  //
 		  std::string readBuffer;
 		  printf("deleting row with pacchetto = 2\n");
-		  system("mongo messages --eval \"db.test.remove({\"packet\":0}\"");
+		  system("mongo messages --eval \"db.test.remove({\"packet\":0})\"");
 		  curl = curl_easy_init();
 		  if (curl) {
 			  curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:28017/messages/test/");
